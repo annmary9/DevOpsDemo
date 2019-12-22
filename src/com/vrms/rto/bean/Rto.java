03cccc9f8b890132b82319abd27c5f442c3617a2
Ann Mary Varghese <annmaryv1993@gmail.com>
2019-12-22 12:22:55 +0530

    initial setup

---
+++ src/com/vrms/rto/bean/Rto.java
@@ -0,0 +1,37 @@
+package com.vrms.rto.bean;
+import javax.validation.constraints.Size;
+public class Rto {
+	private int id;
+	@Size(min=1,message="required") 
+	private String name;    
+	private String postedAt;    
+	
+	private String address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPostedAt() {
+		return postedAt;
+	}
+	public void setPostedAt(String postedAt) {
+		this.postedAt = postedAt;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+
+}
